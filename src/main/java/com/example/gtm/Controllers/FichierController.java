a287c6128327486a948724fe2624e670e2e71924
Kevin Dubuche <kevindubuche@gmail.com>
2021-06-24 11:31:24 -0400

    -

--- src/main/java/com/example/gtm/Controllers/FichierController.java
+++ src/main/java/com/example/gtm/Controllers/FichierController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
 @RestController
 @RequestMapping("/api/file")
 public class FichierController {
